be68eb7a22b920c883e3922ca8f1ef90eef791bb
Doug Shannon <doug@tensure.us>
2019-09-17 18:38:42 -0400

    dougs commit

--- notes/StopWatch.ts
+++ notes/StopWatch.ts
@@ -1,52 +1,46 @@
-
 class stopwatch {
-    // Not sure how to initialize all the parameters to 0;
-    
-    constructor(
-        public startTime: Date,
-        public endTime: Date,
-        public running: boolean,
-        public duration: number
-) {
-    this.startTime = startTime;
-    this.endTime = endTime;
-    this.running = running;
-    this.duration = duration;
+  // Not sure how to initialize all the parameters to 0;
+
+  private startTime: Date;
+  private endTime: Date;
+  private running: boolean;
+  public method = "seconds";
+
+  public start() {
+    if (this.running) {
+      throw new Error("Stopwatch has already started");
     }
+    this.running = true;
+    this.startTime = new Date();
+  }
 
-    start() {
-        if (this.running) {
-            throw new Error('Stopwatch has already started');
-        }
+  public stop(): number {
+    if (!this.running) {
+      throw new Error("Stopwatch is not started.");
+    }
+    this.running = false;
+    this.endTime = new Date();
+    return (this.endTime.getTime() - this.startTime.getTime()) / 1000;
+  }
 
-        this.running = true;
+  public reset() {
+    this.startTime = null;
+    this.endTime = null;
+    this.running = false;
+  }
 
-        this.startTime = new Date();
-    };
-
-    stop() {
-        if (!this.running) {
-            throw new Error('Stopwatch is not started.')
-        };
-
-        this.running = false;
-        
-        this.endTime = new Date();
-
-        const seconds = (this.endTime.getTime() - this.startTime.getTime() / 1000)
-
-        this.duration += seconds;
-    };
-
-    reset() {
-        this.startTime = null;
-        this.endTime = null;
-        this.running = false;
-        this.duration = 0;
-    };
-
-    // are getter/setter applicable in this class?
+  // are getter/setter applicable in this class?
 }
 
+let OURDATA;
 
+fetch("https://api.mockaroo.com/api/0c501690?count=10&key=ec35dd50")
+  .then(data => {
+    return data.json();
+  })
+  .then(data => {
+    OURDATA = data;
+    console.log("inTHEN", data);
+  });
 
+console.log("AFTER FETCH", OURDATA);
