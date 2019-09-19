c8adc74a38440b43a62c3c3dd76a395f178c0561
Rebecca Payne <rebeccapayne@Rebeccas-MacBook.local>
2019-09-18 21:23:13 -0400

    New Challenge

---
+++ notes/18.largestNum.ts
@@ -0,0 +1,11 @@
+
+// function largestNumber(nums) {
+//   return nums.join('').split('').sort().reverse().join('');
+// }
+
+function largestNumber(nums) {
+    
+}
+
+console.log(largestNumber([3,30,34,5,9]));
+console.log(largestNumber([3,50,34,5,9,798]));
