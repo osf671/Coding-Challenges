f702700d66a56509066c724eef155f9017f3421d
Rebecca Payne <rebeccapayne@Rebeccas-MacBook.local>
2019-09-22 22:47:13 -0400

    Solution using Math.max

--- New Challenges/16.longestOfFour.ts
+++ New Challenges/16.longestOfFour.ts
@@ -1,17 +1,27 @@
-function largestOfFour(arr) {
-    let maxValArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        let maxVal = 0;
-        for (let j = 0; j < arr[i].length; j++) {
-            let currentVal = arr[i][j];
-            if (currentVal > maxVal) {
-                maxVal = currentVal;
-            }
-        }
-        maxValArr.push(maxVal);
-    }
-    console.log(maxValArr)
-    return maxValArr;
-}
+// function largestOfFour(arr) {
+//     let maxValArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let maxVal = 0;
+//         for (let j = 0; j < arr[i].length; j++) {
+//             let currentVal = arr[i][j];
+//             if (currentVal > maxVal) {
+//                 maxVal = currentVal;
+//             }
+//         }
+//         maxValArr.push(maxVal);
+//     }
+//     console.log(maxValArr)
+//     return maxValArr;
+// }
 
-largestOfFour([[1, 2, 3, 4], [5, 18, 0, 12], [3, 5, 12, 5], [28, 9, 2, 34]])
+function largestOfFour(arr) {
+    let finalArr = [];
+    let maxVal = 0;
+    for (let arr1 of arr) {
+      maxVal = Math.max(...arr1);
+      finalArr.push(maxVal);
+    }
+    return finalArr;
+  }
+
+console.log(largestOfFour([[1, 2, 3, 4], [5, 18, 0, 12], [3, 5, 12, 5], [28, 9, 2, 34]]))
