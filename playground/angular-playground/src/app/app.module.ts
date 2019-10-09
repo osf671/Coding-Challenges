f72a678a0abef4ff2762fa0e5c875f62ab5d53a3
Ellayna Sara Marie Shannon <Layna@Ellaynas-MacBook-Air.local>
2019-10-09 10:59:59 -0400

    changes

--- playground/angular-playground/src/app/app.module.ts
+++ playground/angular-playground/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaygroundComponent } from './playground/playground.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatStepperModule} from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,15 @@ import { PlaygroundComponent } from './playground/playground.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDividerModule,
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- playground/angular-playground/src/app/playground/playground.component.ts
+++ playground/angular-playground/src/app/playground/playground.component.ts
@@ -13,7 +13,6 @@ export class PlaygroundComponent implements OnInit {
     fetch("https://api.mockaroo.com/api/0c501690?count=10&key=ec35dd50")
       .then(res => res.json())
       .then(incomingData => {
-        debugger;
         this.data = incomingData;
         return incomingData;
       })
