24372974cb55d629fb7e40308d157d77a7956707
Doug Shannon <doug@tensure.us>
2019-10-08 20:51:35 -0400

    more cleaning

--- playground/angular-playground/src/app/app.module.ts
+++ playground/angular-playground/src/app/app.module.ts
@@ -1,58 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PlaygroundComponent } from './playground/playground.component';
-import { Playground1Component } from './playground1/playground1.component';
-import { SummaryPipe } from './summary.pipe';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select'
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatFormFieldControl } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatIconModule, MatIcon } from '@angular/material/icon'
-import { MatButtonModule } from '@angular/material/button';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTooltipModule} from '@angular/material/tooltip'
-import {MatTabsModule} from '@angular/material/tabs'
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PlaygroundComponent } from "./playground/playground.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlaygroundComponent,
-    Playground1Component,
-    SummaryPipe,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    FormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatTabsModule,
-    HttpClientModule
-  ],
-  providers: [
-  ],
+  declarations: [AppComponent, PlaygroundComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
