f814da08fe337dbc907edf97c30012e2dcf9fcd4
Rebecca Payne <rebeccapayne@Rebeccas-MacBook.local>
2019-10-11 05:54:17 -0400

    playground

--- playground/angular-playground/src/app/user-page/user-profile/user-profile.component.ts
+++ playground/angular-playground/src/app/user-page/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  @Input() user: any;
+  @Output() returnClicked: EventEmitter<null> = new EventEmitter;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  returnButtonClicked() {
+    this.returnClicked.emit();
   }
 
 }
